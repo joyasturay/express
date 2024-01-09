ddafa3bbad92a5f2f782b8609cba6e325a7dd7fe
joyasturay <joyasturay1@gmail.com>
2024-01-09 17:29:55 +0530

    second commit

--- index.js
+++ index.js
@@ -14,5 +14,19 @@ app.get("/",(req,res)=>{
     res.send("you contacted root path");
 })
 app.get("/hello",(req,res)=>{
-    res.send("you contacted hello path");
+    res.send("you contacted hello path,Hello");
+})
+/*app.get("*",(req,res)=>{
+    res.send("this path does not exist");
+})*/
+app.get("/:username/:id",(req,res)=>{
+    console.log(req.params);
+    let{username,id}=req.params;
+    res.send(`Welcome to the page of @ ${username} with id ${id}`);
+    res.send("data sent");
+})
+app.get("/search",(req,res)=>{
+    console.log(req.query);
+    let {q}= req.query;
+    res.send(`no results found for : ${q}`);
 })
